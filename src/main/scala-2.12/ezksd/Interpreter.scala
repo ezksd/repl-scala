aae627ca1f11319bb92569043108a1304a9e6f94
ezksd <yangli245@live.com>
2017-02-25 11:26:36 +0800

    fix

--- src/main/scala-2.12/ezksd/Interpreter.scala
+++ src/main/scala-2.12/ezksd/Interpreter.scala
@@ -5,15 +5,14 @@ import ezksd.Parser.parse
 
 object Interpreter {
   type Continuation[T] = T => Unit
-  type BiContinuation[T, U] = (T, U) => Unit
 
   @inline
-  def cps_map(l: List[Any], f: BiContinuation[Any, Continuation[Any]], k: Continuation[List[Any]]){
+  def cps_map(l: List[Any], f: (Any, Any => Unit) => Unit, k: Continuation[List[Any]]) {
     if (l.isEmpty) k(l) else f(l.head, head => cps_map(l.tail, f, tai => k(head :: tai)))
   }
 
   @inline
-  def evalList(list: List[Any], env: Environment, k: Continuation[List[Any]]){
+  def evalList(list: List[Any], env: Environment, k: Continuation[List[Any]]) {
     cps_map(list, (x, k1) => eval(x, env, r => k1(r)), k)
   }
 
@@ -29,7 +28,8 @@ object Interpreter {
         case (key: String) :: value :: Nil => eval(value, env, r => env.define(key, r))
         case (head: List[String]) :: body => env.define(head.head, Closure(env, head.tail, body))
       }
-      case "set!" :: (key: String) :: value :: Nil => env.set(key, value)
+      //case "set!" :: (key: String) :: value :: Nil => env.set(key, value)
+      case "set!" :: (key: String) :: value :: Nil => eval(value, env, r => env.set(key, value))
       case "lambda" :: (params: List[String]) :: xs => cont(Closure(env, params, xs))
       case "call/cc" :: e1 :: Nil => eval(e1, env, {
         case Closure(saved, h :: xs, body) => eval(body, saved.extend(h, cont), cont)
@@ -53,7 +53,7 @@ object Interpreter {
     }
   }
 
-  def main(args: Array[String]){
+  def main(args: Array[String]) {
     val exp = parse("(let ((yin (call/cc (lambda (c) c)))) (display \"#\") (let ((yang (call/cc (lambda (c) c)))) (display \"*\") (yin yang)))")
     exp.foreach(e => eval(e, Environment.env0, println))
   }
