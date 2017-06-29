e986e126d20819b710a0330adff6bdc5d7703ae2
ezksd <yangli245@live.com>
2017-06-29 16:59:02 +0800

    ...

--- src/main/scala/ezksd/Interpreter.scala
+++ src/main/scala/ezksd/Interpreter.scala
@@ -2,7 +2,7 @@ package ezksd
 
 import ezksd.Parser.parse
 
-object Interpreter {
+class Interpreter(env:Environment) {
 
   def cps_map[T](l: List[Any], f: (Any, Any => T) => T, k: List[Any] => T):T = {
     l match {
@@ -11,50 +11,49 @@ object Interpreter {
     }
   }
 
-  def evalList[T](list: List[Any], env: Environment, k: List[Any] => T) : T = {
-    cps_map(list, (x:Any, k1:Any => T) => eval(x, env, r => k1(r)), k)
+  def evalList[T](list: List[Any], k: List[Any] => T) : T = {
+    cps_map(list, (x:Any, k1:Any => T) => eval(x, r => k1(r)), k)
   }
-
-  def evalAndPrint(expr: Any): Unit = eval(expr, Environment.env0, println)
 
   def firstString(list: List[Any]): String = list.head.asInstanceOf[String]
 
-  def eval[T](expr: Any, env: Environment, cont: Any => T) : T =  {
+  def eval[T](expr: Any, cont: Any => T) : T =  {
     expr match {
       case s: String => cont(env.lookup(s))
       case "define" :: xs => xs match {
-        case (key: String) :: value :: Nil => eval(value, env, r => cont(env.define(key, r)))
+        case (key: String) :: value :: Nil => eval(value, r => cont(env.define(key, r)))
         case (head: List[String]) :: body => cont(env.define(head.head, Closure(env, head.tail, body)))
       }
-      case "set!" :: (key: String) :: value :: Nil => eval(value, env, r =>cont(env.set(key, value)))
+      case "set!" :: (key: String) :: value :: Nil => eval(value, r => cont(env.set(key, r)))
       case "lambda" :: (params: List[String]) :: xs => cont(Closure(env, params, xs))
-      case "call/cc" :: e1 :: Nil => eval(e1, env, {
-        case Closure(saved, h :: xs, body) => eval(body, saved.extend(h, Cont(cont)), cont)
+      case "call/cc" :: e1 :: Nil => eval(e1, {
+        case Closure(saved, h :: xs, body) => new Interpreter(saved.extend(h,Cont(cont))).eval(body, cont)
       })
       case "quote" :: value :: Nil => cont(value)
       case "let" :: (list: List[List[Any]]) :: body =>
-        eval(Closure(env, list.map(firstString), body) :: list.map(_.tail.head), env, cont)
+        eval(Closure(env, list.map(firstString), body) :: list.map(_.tail.head), cont)
       case "let" :: (name: String) :: (list: List[List[Any]]) :: body =>
         val closure = Closure(env, list.map(firstString), body)
-        eval(closure :: list.map(_.tail.head), env.extend(name, closure), cont)
-      case "if" :: pred :: first :: second :: Nil => eval(pred, env, {
-        case true => eval(first, env, cont)
-        case false => eval(second, env, cont)
+        new Interpreter(env.extend(name, closure)).eval(closure :: list.map(_.tail.head), cont)
+      case "if" :: pred :: first :: second :: Nil => eval(pred, {
+        case true => eval(first, cont)
+        case false => eval(second, cont)
       })
-      case list: List[Any] => evalList(list, env, {
-        case Closure(saved, p, body) :: v => evalList(body, saved.extend(p, v), r => cont(r.last))
+      case list: List[Any] => evalList(list, {
+        case Closure(saved, p, body) :: v => new Interpreter(saved.extend(p,v)).evalList(body, r => cont(r.last))
         case Primitive(f) :: v => cont(f(v))
         case (k:Cont) :: Nil => cont(k)
         case Cont(k) :: v :: Nil => k(v).asInstanceOf[T]
-//        case c@Cont(k):: v => if(v isEmpty) cont(c) else k(v.head).asInstanceOf[T]
       })
       case _ => cont(expr)
     }
   }
 
+}
+object Interpreter{
+
   def main(args: Array[String]) {
     val exp = parse("(let ((yin (call/cc (lambda (c) c)))) (display \"#\") (let ((yang (call/cc (lambda (c) c)))) (display \"*\") (yin yang)))")
-    exp.foreach(e => eval(e, Environment.env0, println))
+    exp.foreach(e => new Interpreter(Environment.env0).eval(e, println))
   }
-
 }
--- src/main/scala/ezksd/Repl.scala
+++ src/main/scala/ezksd/Repl.scala
@@ -1,18 +1,18 @@
 package ezksd
 
-import ezksd.Interpreter.evalAndPrint
 
 import ezksd.Parser.parse
 
 object Repl {
   def main(args: Array[String]): Unit = {
+    val int = new Interpreter(Environment.env0)
     while (true) {
       val txt = io.StdIn.readLine(">")
       if (txt == null || txt.equals("") || txt.equals("\u0004")) {
         return
       } else {
         try {
-          parse(txt).foreach(evalAndPrint)
+          parse(txt).foreach(x => int.eval(x,print))
         } catch {
           case ex: Ex => print(ex.getMessage)
         }
